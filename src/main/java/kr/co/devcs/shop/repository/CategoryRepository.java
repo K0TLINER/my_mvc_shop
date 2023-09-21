4587514385bb6ea5d94ac8d2364e79798036f6d3
KTH <trends.kts@gmail.com>
2023-09-21 13:56:13 +0900

    feat: CategoryRepository findAllByParentCategoryCategoryId method 추가

--- src/main/java/kr/co/devcs/shop/repository/CategoryRepository.java
+++ src/main/java/kr/co/devcs/shop/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package kr.co.devcs.shop.repository;
 import kr.co.devcs.shop.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByParentCategoryCategoryId(Long parentCategoryId);
 }
