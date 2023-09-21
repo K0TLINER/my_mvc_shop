03aa2f77def5bba1402c3330828faad893795646
KTH <trends.kts@gmail.com>
2023-09-21 13:43:49 +0900

    feat: CategoryService 구현해야 할 method getCategoryListByParentCategoryId 선언(overloading 2개)

--- src/main/java/kr/co/devcs/shop/service/CategoryService.java
+++ src/main/java/kr/co/devcs/shop/service/CategoryService.java
@@ -4,10 +4,13 @@ import kr.co.devcs.shop.dto.CategoryForm;
 import kr.co.devcs.shop.entity.Category;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
 public interface CategoryService {
     void addCategory(CategoryForm categoryForm);
     Optional<Category> getCategory(long categoryId);
+    List<Category> getCategoryListByParentCategoryId();
+    List<Category> getCategoryListByParentCategoryId(long parentCategoryId);
 }
